24be14e2620cf5c7cacef00adbb2f9bf60ebe927
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-20 17:27:43 +0530

    Added 3 functionalities: 1) Forms – build a new article creation form, 2) Create action – save newly created articles, 3) Messaging – validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,18 @@ class ArticlesController < ApplicationController
 		# here we are going to have all the articles collected in one variable
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+     @article = Article.new(params.require(:article).permit(:title, :description)) 
+     if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article   #  redirect_to article_path(@article)
+     else
+      render 'new'    # display new form
+     end
+  end
+
 end
