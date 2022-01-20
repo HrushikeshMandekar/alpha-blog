c191aabbe2087733266e118a23f3909f72e93322
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-20 20:09:16 +0530

    Added the edit and update existing article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
      @article = Article.new(params.require(:article).permit(:title, :description)) 
      if @article.save
@@ -22,6 +26,16 @@ class ArticlesController < ApplicationController
      else
       render 'new'    # display new form
      end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 end
