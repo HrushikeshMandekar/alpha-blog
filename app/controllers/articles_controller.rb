db25bdc32311e143e9cff492579bbc865e55adc9
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-21 19:36:29 +0530

    Removed the redundancies from the code with help of partial and private method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     # byebug    # for debugging
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -15,11 +16,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-     @article = Article.new(params.require(:article).permit(:title, :description)) 
+     @article = Article.new(article_params) 
      if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article   #  redirect_to article_path(@article)
@@ -29,8 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -39,9 +38,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
