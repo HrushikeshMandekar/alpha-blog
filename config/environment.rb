a673608bbe6e11a7a95cce4b49a0d21931a925cf
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-22 21:12:27 +0530

    Styled validation and flash messages, and also show view

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# To get rid of field error setting, this error was distrubing our layout
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
+# do not try to understand above code
