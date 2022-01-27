40972eb61f92d30c73176d3e50795fdec1f64f4c
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-27 15:46:58 +0530

    Added the Admin functionality and all the permissions to it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to root_path
   end
@@ -56,9 +56,9 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit and update your profile!"
-      redirect_to user_path(@user)
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit and delete your profile!"
+      redirect_to @user
     end
   end
 
