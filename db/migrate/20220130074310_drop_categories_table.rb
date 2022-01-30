7f75fd6ff830852b356e8e2e2388c8552df84b76
HrushikeshMandekar <mounthitler@gmail.com>
2022-01-30 19:17:04 +0530

    deleted one migration file named drop_categories_table.rb

--- db/migrate/20220130074310_drop_categories_table.rb
+++
@@ -1,9 +0,0 @@
-class DropCategoriesTable < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :article_categories
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
